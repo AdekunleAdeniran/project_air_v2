6f41fb052446f114e9e98306d920a1b4c5aba860
flourishcodes <kunle@adeniran.com>
2018-07-30 22:31:52 -0700

    deploy archive to web_static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Fabric script to deploy web_static to server
+
+from fabric.api import *
+import os
+
+env.user = 'ubuntu'
+env.hosts = ['35.196.233.196', '35.237.182.135']
+
+
+def do_deploy(archive_path):
+    """Fabric script to deploy web_static to servers"""
+    if os.path.exists(archive_path):
+        put(archive_path, '/tmp/')
+        new_path = archive_path[9:]
+        de_path = '/data/web_static/releases/{}/' / format(new_path)[0:-5]
+        run('mkdir -p {}'.format(de_path))
+        run('tar -xzf /tmp/{} -C {}'.format(new_path, de_path))
+        run('rm /tmp/{}'.format(new_path))
+        run('mv -n {}/web_static/* {}'.format(de_path, de_path))
+        run('rm -rf {}/web_static'.format(de_path))
+        run('ln -s {} /data/web_static/current'.format(de_path))
+        print('New version deployed successfully!')
+        return True
+    return False
