dbad937736ff78cb42f01a4d6515a5602f98e3a7
flourishcodes <kunle@adeniran.com>
2018-08-14 21:53:27 -0700

    Update init in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -44,10 +44,10 @@ class BaseModel:
         else:
             try:
                 time_format = "%Y-%m-%dT%H:%M:%S.%f"
-                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                         "%Y-%m-%dT%H:%M:%S.%f")
-                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                         "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
             except KeyError:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
